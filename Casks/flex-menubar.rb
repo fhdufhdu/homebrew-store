70509f512d2bfb8e7d36fb2a42810519b14de036
fhdufhdu <fhdufhdu@gmail.com>
2026-09-08 01:04:29 +0000

    flex-menubar 1.0.10

--- Casks/flex-menubar.rb
+++ Casks/flex-menubar.rb
@@ -1,8 +1,8 @@
 # 이 파일은 tap 레포(fhdufhdu/homebrew-store)의 Casks/flex-menubar.rb 의 참조 사본입니다.
 # 실제 배포 시 version/sha256 은 .github/workflows/release.yml 가 tap 레포 쪽을 자동 갱신합니다.
 cask "flex-menubar" do
-  version "1.0.9"
-  sha256 "e2b48623591762a4f8316439632dcb636216c7263ab96eb7865aa9198587b0ec"
+  version "1.0.10"
+  sha256 "492e056aad32c3da17b583b8dd0ee762c4a95c085b949b95a80297dccde44ec7"
 
   url "https://github.com/fhdufhdu/homebrew-store/releases/download/flex-menubar-v#{version}/flex-menubar.zip"
   name "flex-menubar"
